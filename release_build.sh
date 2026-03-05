5b98b9e8b9e29bf56ea6c8d69d3eb77286b2090a
ideskangel.m <dev@deskangel.com>
2026-03-05 20:47:07 +0800

    + added apk build in build script

--- release_build.sh
+++ release_build.sh
@@ -9,6 +9,9 @@ BUILD_RESULT=0
 for optname in "$@"
 do
     case $optname in
+        apk)
+            OPT_APP_FORMAT="apk"
+            ;;    
         macos)
             OPT_APP_FORMAT="macos"
             ;;
@@ -44,13 +47,22 @@ if [[ "$OPT_APP_FORMAT" == "macos" ]]; then
     fvm flutter build macos --release
     BUILD_RESULT=$?
 
-    if [[ $BUILD_RESULT != 0 ]]; then
-        clrecho "Failed to build the $OPT_APP_FORMAT"
-    fi
+elif [[ "$OPT_APP_FORMAT" == "apk" ]]; then    
+    fvm flutter build apk --obfuscate --release --split-per-abi --split-debug-info=debug_info/
+    BUILD_RESULT=$?
 fi
 
-if [[ $BUILD_RESULT == 0 && $OPT_DEPLOYMENT == "-d" ]]; then
-    echo "deploy to /Applications/"
-    rm -rf /Applications/AnxReader.app
-    cp -rf "$APP_PATH" /Applications/AnxReader.app
+if [[ $BUILD_RESULT != 0 ]]; then
+    clrecho "Failed to build the $OPT_APP_FORMAT"
+    exit 1
+fi
+
+if [[ $OPT_DEPLOYMENT == "-d" ]]; then
+    if [[ "$OPT_APP_FORMAT" == "macos" ]]; then
+        echo "deploy to /Applications/"
+        rm -rf /Applications/AnxReader.app
+        cp -rf "$APP_PATH" /Applications/AnxReader.app
+    else
+        scp2p30p build/app/outputs/apk/release/app-arm64-v8a-release.apk /root/download/anx-reader.apk
+    fi
 fi
